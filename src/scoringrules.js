5178eb0ff7d6590ea345606dc246e7efd0257dd0
Mike Hodnick <mike.hodnick@gmail.com>
2013-06-03 09:16:59 -0500

    changed level bonus scoring rules

--- src/scoringrules.js
+++ src/scoringrules.js
@@ -17,7 +17,7 @@
     },
 
     pointsForLevel: function(level){
-      var base = 500 * level.number;
+      var base = 100 * level.number;
       if (level.shots === 0) return base;
       var percent = level.asteroidsShot / level.shots;
       return base + Math.floor(percent * 1000);
